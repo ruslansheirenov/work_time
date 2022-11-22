3b869c89fd9b4a787fba24d6e52f349673d8f54c
ruslansheirenov <ruslansheirenov@gmail.com>
2022-11-22 13:07:58 +0600

    Added pagination

--- source/worktime/settings.py
+++ source/worktime/settings.py
@@ -135,6 +135,9 @@ AUTH_USER_MODEL = 'accounts.User'
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5, 
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
